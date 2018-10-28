d53fee5a39c119d488400f6ab6b94869d4670709
Phil Lucks <philip.lucks@snapengage.com>
2018-10-28 07:49:07 -0600

    some webpack4 updates

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,6 +1,5 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -46,6 +45,18 @@ module.exports = {
                   }
                 }
               }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "[name].[ext]"
             }
           }
         ]
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 const extractSass = new ExtractTextPlugin({
   filename: "css/style.css"
 });
@@ -18,7 +17,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/"
   },
-
   // Change to production source maps
   devtool: "source-map",
   module: {
@@ -42,10 +40,25 @@ module.exports = {
           // use style-loader in development
           fallback: "style-loader"
         })
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Production"
+    }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production")
@@ -63,14 +76,6 @@ module.exports = {
     new ExtractTextPlugin({
       filename: "styles/styles.css",
       allChunks: true
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        output: {
-          comments: false
-        }
-      }
     })
   ],
   optimization: {
